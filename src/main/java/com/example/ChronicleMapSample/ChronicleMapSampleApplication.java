0e32818ecf96c74b2c748a2d5b8a4ec49b82cf47
Grigor Lekarov <grigor.lekarov@cumulocity.com>
2024-06-04 12:57:00 +0200

    Project skeleton

---
+++ src/main/java/com/example/ChronicleMapSample/ChronicleMapSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChronicleMapSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronicleMapSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronicleMapSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ChronicleMapSample/ChronicleMapSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChronicleMapSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronicleMapSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
